c088ac75d85210888ef742b231c582e16370ba42
Alberto Troisi <albytroisi@gmail.com>
2019-07-24 09:07:12 +0000

    Azure AD oauth config

--- contrib/docker/superset_config.py
+++ contrib/docker/superset_config.py
@@ -15,7 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 import os
-
+from flask_appbuilder.security.manager import AUTH_OID, AUTH_REMOTE_USER, AUTH_DB, AUTH_LDAP, AUTH_OAUTH
 
 def get_env_variable(var_name, default=None):
     """Get the environment variable or raise exception."""
@@ -46,6 +46,55 @@ SQLALCHEMY_DATABASE_URI = 'postgresql://%s:%s@%s:%s/%s' % (POSTGRES_USER,
 REDIS_HOST = get_env_variable('REDIS_HOST')
 REDIS_PORT = get_env_variable('REDIS_PORT')
 
+# ----------------------------------------------------
+# AUTHENTICATION CONFIG
+# ----------------------------------------------------
+# The authentication type
+# AUTH_OID : Is for OpenID
+# AUTH_DB : Is for database (username/password()
+# AUTH_LDAP : Is for LDAP
+# AUTH_REMOTE_USER : Is for using REMOTE_USER from web server
+#AUTH_TYPE = AUTH_DB
+
+# Uncomment to setup Full admin role name
+AUTH_ROLE_ADMIN = 'Admin'
+
+# Uncomment to setup Public role name, no authentication needed
+# AUTH_ROLE_PUBLIC = 'Public'
+
+# Will allow user self registration
+AUTH_USER_REGISTRATION = True
+
+# The default user self registration role
+AUTH_USER_REGISTRATION_ROLE = "Public"
+#AUTH_USER_REGISTRATION_ROLE = "Admin"
+
+#AUTH CONFIG LDAP AND DB AUTH cannot be used both at the same time
+# When using Local DB AUTH uncomment following parameter and comment "AUTH_LDAP"
+#AUTH_TYPE = AUTH_DB
+
+# When using LDAP Auth, setup the ldap server
+AUTH_TYPE = AUTH_OAUTH
+OAUTH_PROVIDERS = [
+  {
+    'name': 'azure',
+    'icon': 'fa-windows',
+    'token_key': 'access_token',
+    'remote_app': {
+      'base_url': 'https://login.microsoftonline.com/common/oauth2/v2.0/',
+      'request_token_params': {
+#        'scope': 'User.read name preferred_username email profile'
+        'scope': 'openid'
+#        'resource': os.environ.get("AZURE_APPLICATION_ID")
+      },
+      'request_token_url': None,
+      'access_token_url': 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
+      'authorize_url': 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
+      'consumer_key': os.environ.get("AZURE_APPLICATION_ID"),
+      'consumer_secret': os.environ.get("AZURE_SECRET")
+    }
+  }
+]
 
 class CeleryConfig(object):
     BROKER_URL = 'redis://%s:%s/0' % (REDIS_HOST, REDIS_PORT)
